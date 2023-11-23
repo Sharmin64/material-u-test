ec6d2bdf1570a499acaa7a6d08fee7a8a8244d32
Sharmin64 <lizaeatsbanana@gmail.com>
2023-11-23 14:08:07 +0600

    component updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import {AppBar, Toolbar} from "@mui/material";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <AppBar>
+      <Toolbar>Navbar will render here.</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
