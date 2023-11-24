e01f191b112dad9aafe6edb543d61e183fa3454d
Sharmin64 <lizaeatsbanana@gmail.com>
2023-11-24 15:08:11 +0600

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,38 @@
-import {AppBar, Toolbar} from "@mui/material";
+import styled from "@emotion/styled";
+import {Castle} from "@mui/icons-material";
+import {AppBar, Box, Toolbar, Typography} from "@mui/material";
 import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({theme}) => ({
+  backgroundColor: "lightsteelblue",
+  padding: "0 10px",
+  width: "40%",
+  borderEndStartRadius: "10px",
+  borderStartEndRadius: "10px",
+}));
+const Icon = styled(Box)(({theme}) => ({
+  backgroundColor: "green",
+  padding: "0 6px",
+  borderEndStartRadius: "8px",
+  borderStartEndRadius: "8px",
+}));
 
 const Navbar = () => {
   return (
-    <AppBar>
-      <Toolbar>Navbar will render here.</Toolbar>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h5" sx={{display: {xs: "none", sm: "block"}}}>
+          Liza Dev
+        </Typography>
+        <Castle sx={{display: {xs: "block", sm: "none"}}} />.
+        <Search>Search </Search>
+        <Icon>Icon</Icon>
+      </StyledToolbar>
     </AppBar>
   );
 };
